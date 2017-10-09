8a993012e40398841b7233972704ce724f293d05
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-10-09 17:23:27 +0300

    add star classes

--- src/Star/Star.java
+++ src/Star/Star.java
@@ -1,4 +1,54 @@
 package Star;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Circle;
+import static Star.StarClient.*;
 
-public class Star {
+public class Star extends Application {
+    public static Circle circle = new Circle(x,y,r);
+
+    public static void main(String...args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        initUI(stage);
+    }
+
+    private void initUI(Stage stage) {
+
+        Pane root = new Pane();
+
+        Canvas canvas = new Canvas(600, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawStarShape(gc);
+
+        root.getChildren().add(canvas);
+
+        Scene scene = new Scene(root, 600, 600, Color.WHITESMOKE);
+
+        stage.setTitle("Star");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void drawStarShape(GraphicsContext gc) {
+        double x = circle.getCenterX();
+        double y = circle.getCenterY();
+        double r = circle.getRadius();
+
+        double xpoints[] = {x - r * 0.23, x, x + r * 0.23, x + r * 0.95, x + r * 0.36,
+                x + r * 0.59, x, x - r * 0.59, x - r * 0.36, x - r * 0.95};
+        double ypoints[] = {y - r * 0.31, y - r, y - r * 0.31, y - r * 0.31, y + r * 0.09,
+                y + r * 0.75, y + r * 0.34, y + r * 0.75, y + r * 0.09, y - r * 0.31};
+        gc.strokePolygon(xpoints, ypoints, xpoints.length);
+    }
 }
--- src/Star/StarClient.java
+++ src/Star/StarClient.java
@@ -1,4 +1,37 @@
 package Star;
+import java.util.Scanner;
 
 public class StarClient {
+    static int x, y, r, n;
+
+    public static void main(String[] args) {
+        System.out.print("Star radius (100-350):\t\t\t\t\t");
+        r = scanner();
+        while (r < 100 || r > 350) {
+            System.out.println("Error!");
+            r = scanner();
+        }
+        System.out.print("Coordinate x of star center (till 350):\t");
+        x = scanner();
+        while (x < r || x > 350) {
+            System.out.println("Error");
+            x = scanner();
+        }
+        System.out.print("Coordinate y of star center (till 350):\t");
+        y = scanner();
+        while (y < r || y > 350) {
+            System.out.println("Error!");
+            y = scanner();
+        }
+        Star.main();
+    }
+
+    public static int scanner() {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextInt()) {
+            n = s.nextInt();
+            break;
+        }
+        return n;
+    }
 }
